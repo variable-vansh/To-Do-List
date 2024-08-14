6f2d204b28072004aed46c65f39e60cb8363343f
Vansh Mishra <144242983+variable-vansh@users.noreply.github.com>
2024-08-15 02:19:34 +0530

    project functionality added, neatness imporved

--- src/index.js
+++ src/index.js
@@ -1,53 +1,71 @@
-let taskCounter = 0;
-taskArray = [];
-
 class Task {
-    // a todo item has: title, description, dueDate, priority
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.taskNumber = taskCounter + 1;
-        taskCounter++;
+        this.taskNumber = ++Task.taskCounter;
     }
 
     viewTasks() {
-        console.log(`Task number: ${this.taskNumber}`)
-        console.log(`Title: ${this.title}`)
-        console.log(`Description: ${this.description}`)
-        console.log(`Due Date: ${this.dueDate}`)
-        console.log(`Priority: ${this.priority}`)
+        console.log(`Task number: ${this.taskNumber}`);
+        console.log(`Title: ${this.title}`);
+        console.log(`Description: ${this.description}`);
+        console.log(`Due Date: ${this.dueDate}`);
+        console.log(`Priority: ${this.priority}`);
+    }
+
+    static resetTaskCounter() {
+        Task.taskCounter = 0;
     }
 }
 
-taskArray.push(new Task("<demo title>", "<demo description>", "<demo due date>", "<demo priority>"))
-taskArray.push(new Task("<demo title 2>", "<demo description>", "<demo due date>", "<demo priority>"))
-taskArray.push(new Task("<demo title 3>", "<demo description>", "<demo due date>", "<demo priority>"))
+Task.taskCounter = 0;
 
-// const task1 = new Task("<demo title>", "<demo description>", "<demo due date>", "<demo priority>");
-for (let task of taskArray) {
-    task.viewTasks();
-    console.log("----------------------------------------")
+class Project {
+    constructor(tasks) {
+        this.tasks = [...tasks];
+        this.projectNumber = ++Project.projectCounter;
+    }
+
+    viewProject() {
+        console.log(`Project-${this.projectNumber}`);
+        this.tasks.forEach(task => task.viewTasks());
+        console.log("-------------------------------------");
+    }
 }
 
+Project.projectCounter = 0;
 
+class ProjectCollection {
+    constructor(projects) {
+        this.projects = projects;
+    }
 
+    viewProjectCollection() {
+        this.projects.forEach(project => project.viewProject());
+    }
+}
 
+// Usage
+const taskArray1 = [
+    new Task("<demo title>", "<demo description>", "<demo due date>", "<demo priority>"),
+    new Task("<demo title 2>", "<demo description>", "<demo due date>", "<demo priority>"),
+];
 
+//task counter reset to 0 after filling in the first task, so that when next task is created, the numbering starts afresh
+Task.resetTaskCounter()
 
+const firstProject = new Project(taskArray1);
+const projectsArray = [firstProject];
 
+const taskArray2 = [
+    new Task("<demo title 3>", "<demo description>", "<demo due date>", "<demo priority>"),
+    new Task("<demo title 4>", "<demo description>", "<demo due date>", "<demo priority>"),
+];
 
+const secondProject = new Project(taskArray2);
+projectsArray.push(secondProject);
 
-
-
-
-
-
-
-
-
-//this was just a demo import to test webpack setup
-// import _ from 'lodash';
-// alert(_.join(['lol', 'idk'], '-'))
-// i changed code in my index.js file from src folder, but it got moved to main.js of dist as the program was instructed to do so by webpack.config.js
+const projectCollection = new ProjectCollection(projectsArray);
+projectCollection.viewProjectCollection();
